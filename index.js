deb178ef4dac5f5d3b1b01806b2e24909e0b1472
Fred Lin <gasolin@gmail.com>
2017-11-09 17:22:14 +0800

    set isDirty = true by default

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ var postcss = require('postcss');
 
 const DEBUG = false;
 const SUPPORT_PROPS = ['float', 'clear', 'text-align', 'padding-inline-start', 'padding-inline-end',
-                       'border-inline-start', 'border-inline-end', 'border-inline-start-color', 'border-inline-end-color',
-                       'border-inline-start-style', 'border-inline-end-style',
-                       'border-inline-start-width', 'border-inline-end-width',
-                       'border-inline-start-top', 'border-inline-end-top',
-                       'border-top-inline-start-radius', 'border-top-inline-end-radius',
-                       'border-bottom-inline-start-radius', 'border-bottom-inline-end-radius',
-                       'margin-inline-start', 'margin-inline-end',
-                       'offset-inline-start', 'offset-inline-end'
-                      ];
+    'border-inline-start', 'border-inline-end', 'border-inline-start-color', 'border-inline-end-color',
+    'border-inline-start-style', 'border-inline-end-style',
+    'border-inline-start-width', 'border-inline-end-width',
+    'border-inline-start-top', 'border-inline-end-top',
+    'border-top-inline-start-radius', 'border-top-inline-end-radius',
+    'border-bottom-inline-start-radius', 'border-bottom-inline-end-radius',
+    'margin-inline-start', 'margin-inline-end',
+    'offset-inline-start', 'offset-inline-end'
+];
 
 function log(...args) {
     (args || []).forEach(arg => {
@@ -28,7 +28,7 @@ function cloneItem(item) {
 
 // process LTR by default, process RTL when reverseFlag is set
 function processProps(decl, reverseFlag) {
-    let isDirty = false;
+    let isDirty = true;
     let start = !reverseFlag ? 'left' : 'right';
     let end = !reverseFlag ? 'right' : 'left';
     switch (decl.prop.toLowerCase()) {
@@ -41,82 +41,64 @@ function processProps(decl, reverseFlag) {
             } else {
                 decl.value = end;
             }
-            isDirty = true;
         }
         break;
     case 'padding-inline-start':
         decl.prop = 'padding-' + start;
-        isDirty = true;
         break;
     case 'padding-inline-end':
         decl.prop = 'padding-' + end;
-        isDirty = true;
         break;
     case 'border-inline-start':
         decl.prop = 'border-' + start;
-        isDirty = true;
         break;
     case 'border-inline-end':
         decl.prop = 'border-' + end;
-        isDirty = true;
         break;
     case 'border-inline-start-color':
         decl.prop = 'border-' + start + '-color';
-        isDirty = true;
         break;
     case 'border-inline-end-color':
         decl.prop = 'border-' + end + '-color';
-        isDirty = true;
         break;
     case 'border-inline-start-width':
         decl.prop = 'border-' + start + '-width';
-        isDirty = true;
         break;
     case 'border-inline-end-width':
         decl.prop = 'border-' + end + '-width';
-        isDirty = true;
         break;
     case 'border-inline-start-style':
         decl.prop = 'border-' + start + '-style';
-        isDirty = true;
         break;
     case 'border-inline-end-style':
         decl.prop = 'border-' + end + '-style';
-        isDirty = true;
         break;
     case 'border-top-inline-start-radius':
         decl.prop = 'border-top-' + start + '-radius';
-        isDirty = true;
         break;
     case 'border-top-inline-end-radius':
         decl.prop = 'border-top-' + end + '-radius';
-        isDirty = true;
         break;
     case 'border-bottom-inline-start-radius':
         decl.prop = 'border-bottom-' + start + '-radius';
-        isDirty = true;
         break;
     case 'border-bottom-inline-end-radius':
         decl.prop = 'border-bottom-' + end + '-radius';
-        isDirty = true;
         break;
     case 'margin-inline-start':
         decl.prop = 'margin-' + start;
-        isDirty = true;
         break;
     case 'margin-inline-end':
         decl.prop = 'margin-' + end;
-        isDirty = true;
         break;
     case 'offset-inline-start':
         decl.prop = start;
-        isDirty = true;
         break;
     case 'offset-inline-end':
         decl.prop = end;
-        isDirty = true;
         break;
     default:
+        isDirty = false;
         break;
     }
 
